79912523870da99aec32eb047cc65d5b1d089362
DIOGENES JUNIOR <contato@diogenesjunior.com.br>
2016-08-19 22:09:45 -0300

    Atualizações versão 19 de agosto
    Atualizações versão 19 de agosto

--- js/teste.php
+++ js/teste.php
@@ -1,17 +1,11 @@
-[
-    {
-        "id": "mysql-in-a-nutshell",
-        "value": "MySQL in a Nutshell",
-        "label": "<span class=\"hl_results\">MySQL</span> in a Nutshell"
-    },
-    {
-        "id": "how-to-use-php-improved-mysqli-extension-and-why-you-should",
-        "value": "How to Use PHP Improved MySQLi extension (and Why You Should)",
-        "label": "How to Use PHP Improved <span class=\"hl_results\">MySQL</span>i extension (and Why You Should)"
-    },
-    {
-        "id": "is-it-time-to-remove-mysql-in-favor-of-mariadb-in-production-servers",
-        "value": "Is it time to Remove MySQL in favor of MariaDB in Production Servers?",
-        "label": "Is it time to Remove <span class=\"hl_results\">MySQL</span> in favor of MariaDB in Production Servers?"
-    }
-]
+<?php 
+
+$teste = $_POST["teste"];
+$teste2 = $_POST["teste2"];
+$endereco = $_POST["endereco"];
+
+echo "Valor teste: ", $teste, "<br>";
+echo "Valor teste 2: ", $teste2, "<br>";
+echo "Valor teste 3: ", $endereco["cep"], "<br>";
+
+ ?>
