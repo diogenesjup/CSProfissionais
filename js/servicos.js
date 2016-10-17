908105344be072a870e7cdcb0d91459b66e72a6d
DIOGENES JUNIOR <contato@diogenesjunior.com.br>
2016-10-17 18:30:12 -0200

    teste 3
    teste 3

--- js/servicos.js
+++ js/servicos.js
@@ -3918,11 +3918,30 @@ function exitAppPopup() {
 }
 
 
-    //window.plugin.notification.local.add({ id: 1, message: 'Great app!' });
-    navigator.notification.alert('Aqui funciona melhor',  // message
-                 null,    // callback
-                null,    // title
-                 'Ok'     // buttonName
-                 );     
-    alert("funciona se for por aqui");
+    window.plugin.notification.local.add({ id: 1, message: 'Great app!' });
 
+schedule = function () {
+                cordova.plugins.notification.local.schedule({
+                    id: 1,
+                    text: 'Test Message 1',
+                    icon: 'http://3.bp.blogspot.com/-Qdsy-GpempY/UU_BN9LTqSI/AAAAAAAAAMA/LkwLW2yNBJ4/s1600/supersu.png',
+                    smallIcon: 'res://cordova',
+                    sound: null,
+                    data: { test: id }
+                });
+            };
+scheduleDelayed = function () {
+                var now = new Date().getTime(),
+                    _5_sec_from_now = new Date(now + 5 * 1000);
+
+                var sound = device.platform == 'Android' ? 'file://sound.mp3' : 'file://beep.caf';
+
+                cordova.plugins.notification.local.schedule({
+                    id: 1,
+                    title: 'Scheduled with delay',
+                    text: 'Test Message 1',
+                    at: _5_sec_from_now,
+                    sound: sound,
+                    badge: 12
+                });
+            };
